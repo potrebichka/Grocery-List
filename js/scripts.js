c91a4ac239f21c5d6161bb2b9fe23ecf9a77bffa
Nina Potrebich and Trevor Mackin <trevor@>
2019-10-28 13:55:07 -0700

    add grocery list project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-
-
-
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    $("#form1").hide();
+    var arr =[];
+    [1,2,3,4,5].forEach(function(num) {
+      arr.push($("#item" + num).val().toUpperCase())
+    });
+    arr.sort();
+    arr.forEach(function(item) {
+      $("#list").append("<li>" + item + "</li>");
+    })
+  })
 });
